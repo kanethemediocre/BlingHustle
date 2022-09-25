9640121abed74bceec32a909f37ee9aca74170ef
kanethemediocre <kanethemediocre@gmail.com>
2022-09-25 14:20:47 -0500

    quiz and quizblock structures

---
+++ Bling Hustle 452/quiz.js
@@ -0,0 +1,55 @@
+class Quiz{
+	constructor(qtype, qtitle){//type == "blaster" or "upgrade" or "mission"
+		this.type = qtype;
+		this.challenges = [];
+		this.passtolerance = 2;
+		this.title = qtitle;
+		this.q = 0; //Which question in the quiz are you working on
+		}
+	addaddquestion(digits,addendnum,allownegative){
+		var newchallenge = new Challenge("arithmatic");
+		newchallenge.makeaddquestion(digits,addendnum,allownegative);
+		newchallenge.addnumline();
+		this.challenges.push(newchallenge);
+		console.log(this.challenges.length);
+		}
+	addsubquestion(digits,addendnum,allownegative,allownegativeanswer){
+		var newchallenge = new Challenge("arithmatic");
+		newchallenge.makesubquestion(digits,addendnum,allownegative,allownegativeanswer);
+		//newchallenge.addnumline();
+		this.challenges.push(newchallenge);	
+		console.log(this.challenges.length);		
+		}
+	addmultquestion(digits,addendnum,allownegative){
+		var newchallenge = new Challenge("arithmatic");
+		newchallenge.makemultquestion(digits,addendnum,allownegative);
+		newchallenge.multplot();
+		this.challenges.push(newchallenge);
+		console.log(this.challenges.length);		
+		}
+	scramble(){
+		var i = this.challenges.length - 1;
+		while(i > 0){
+	        var j = Math.floor(Math.random() * (i + 1));
+			var temp = this.challenges[i];
+			this.challenges[i] = this.challenges[j];
+			this.challenges[j] = temp;
+			i--;
+			}
+		}
+	draw(x,y){
+		console.log(this.challenges.length);
+		this.challenges[this.q].draw(x,y);
+		this.challenges[this.q].drawplots(x+200,y+250);
+		var i=1;
+		while(this.q-i>=0){
+			//console.log("itried");
+			this.challenges[this.q-i].draw(x,y-24*(i));
+			i++
+			}
+		}
+    }
+var testquiz = new Quiz("test math","w00 w00");
+testquiz.addaddquestion(1,2,false);
+testquiz.addsubquestion(2,2,false,true);
+testquiz.addmultquestion(1,2,false);
