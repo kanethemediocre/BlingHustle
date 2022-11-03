61a7448004bcba08a313978dc0f1b3ce5e42acc3
kanethemediocre <kanethemediocre@gmail.com>
2022-11-02 22:06:03 -0500

    NPC class fully implemented AFAIK

---
+++ Bling Hustle 511/controls.js
@@ -0,0 +1,19 @@
+var controltext = [
+"[B] key will cycle through booster options and [G] key will fire current booster.  ",
+"[A] key toggles autopilot, best used in big empty spaces",
+"[Spacebar] skips current radio message, [J] shows journal of all received messages.  ",
+"[Q] key brings up a quick selection menu for planets.  Click one to select it with your nav computer." ,
+"[N] key cycles navigation modes (Planets, Stations, or None), [<] and [>] cycle navigation targets. " ,
+"[M] key cycles map modes (off, corner, fullscreen), [+] and [-] keys on keypad zoom in and out. " ,
+"[C] key controls the Omnicompass, which can display planet and station indicators, ship indicators, or both." ,
+"[0]-[9] keys select the corresponding weapon, left-mouse-button fires the current weapon.  ",
+"Arrow keys, the mouse wheel, and clickable buttons can also be used to change weapon, including selecting weapons 10-19.",
+"[Z] key cycles between information about current blaster, current cargo, current ship, and off.  ",
+"[Backspace] changes shop pages (Buy, Sell, or Work), [Enter] takes highlighted option,  [Up] and [Down] keys cycle though options on both shop and journal menus.  ",
+"[P] cycles probe modes, and [Delete] activates cheat mode.  [S] toggles starfield effect, which might help slow computers. ",
+"[K] saves current progress, [L] loads current save file.  Very WIP Use the buttons below to download or upload save files. ",
+"[E] key toggles the educational module, where players can complete quizzes to gain bonuses for their ship.  Also very WIP",
+];
+
+			
+			
